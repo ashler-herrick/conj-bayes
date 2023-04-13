937cf9a4606d936070b3afecb883464743995c4b
ashler-herrick <124288133+ashler-herrick@users.noreply.github.com>
2023-04-12 20:19:26 -0400

    Added geometric model

--- pybayes/_discrete.py
+++ pybayes/_discrete.py
@@ -172,8 +172,8 @@ class poisson(model):
         data = np.asarray(data)
         n = len(data)
 
-        self.alpha_n = self.alpha_0 + self.r * n
-        self.beta_n = self.beta_0 + np.sum(data)
+        self.alpha_n = self.alpha_0 + np.sum(data)
+        self.beta_n = self.beta_0 + n
 
     def posterior_mode(self):
         return (self.alpha_n - 1)/self.beta_n
@@ -274,7 +274,8 @@ class multinomial(model):
         random_state = np.random.RandomState(seed)
         p = random_state.dirichlet(self.alpha_n)
         return random_state.choice(np.ones(len(self.alpha_n)), p = p)
-    
+
+#TODO: finish hypergeometric 
 class hypergeometric(model):
 
     def update_model(self, data, **params):
@@ -284,6 +285,58 @@ class hypergeometric(model):
         data = np.asarray(data)
 
         self.alpha_n = self.alpha_0 + np.sum(data,axis = 0)
+
+class geometric(model):
+
+    def update_model(self, data, **params):
+        super()._update_model(**params)
+        self._check_params(['alpha_0','beta_0'])
+
+        data = np.asarray(data)
+        n = len(data)
+        self.alpha_n = self.alpha_0 + n
+        self.beta_n = self.beta_0 + np.sum(data)
+
+    def posterior_mode(self):
+        return (self.alpha_n - 1)/(self.alpha_n + self.beta_n - 2)
+    
+    def posterior_mean(self):
+        return self.alpha_n/(self.alpha_n + self.beta_n)
+    
+    def sample_posterior(self, n = 1, seed = None):
+        return stats.beta.rvs(a = self.alpha_n, b = self.alpha_n, size = n, random_state = seed)
+    
+    def sample_prior_predictive(self, n = 1, seed = None):
+        self._check_params(['alpha_0','beta_0'])
+        random_state = np.random.RandomState(seed)
+        p = random_state.beta(a = self.alpha_0, b = self.beta_0, size = n)
+        return random_state.geometric(p = p, size = n)
+
+    def sample_posterior_predictive(self, n = 1, seed = None):
+        random_state = np.random.RandomState(seed)
+        p = random_state.beta(a = self.alpha_n, b = self.beta_n, size = n)
+        return random_state.geometric(p = p, size = n)
+    
+    def plot(self, plot_type = None):
+        check = self._check_plot(['alpha_0','beta_0','alpha_n','beta_n'], plot_type)
+        x = np.linspace(0,1,100)
+        if self.plot_type == 'prior':
+            plt.plot(x,stats.beta.pdf(x, a = self.alpha_0, b = self.beta_0))
+        
+        if self.plot_type == 'posterior':
+            plt.plot(x,stats.beta.pdf(x, a = self.alpha_n, b = self.beta_n))
+
+        if self.plot_type == 'both':
+            plt.plot(x,stats.beta.pdf(x, a = self.alpha_0, b = self.beta_0))
+            plt.plot(x,stats.beta.pdf(x, a = self.alpha_n, b = self.beta_n))
+
+
+        plt.xlabel('p')
+        plt.ylabel('pdf')
+        plt.legend()
+        plt.title("Distribution of p")
+        plt.show()
+    
 
 
 
